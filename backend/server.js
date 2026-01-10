057b51f4590e90b67156f8aebaf04ccfeb8da3fe
Anubhav Maurya <anubhavmauryaph@gmail.com>
2026-01-11 03:33:11 +0530

    corns job added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 import express from "express";
 import prisma from "./lib/prisma.js";
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 import transactionsRoute from "./routes/transaction.routes.js";
 import rateLimiter from "./middleware/ratelimiter.middleware.js";
 import authRoutes from "./routes/auth.routes.js"
 import { connectRedis } from "./lib/redis.js";
+import job from "./config/cron.js";
+
 
 app.use(rateLimiter)
 app.use(express.json());
+if (process.env.NODE_ENV === "production") job.start();
+
 await connectRedis()
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
 app.use("/api/auth", authRoutes);
