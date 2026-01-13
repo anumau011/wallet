d20263a1818ebd62861e42b9a2988aac4d8f15e5
Anubhav Maurya <anubhavmauryaph@gmail.com>
2026-01-13 22:07:22 +0530

    postgres added

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,17 +1,26 @@
-import 'dotenv/config'
+// import 'dotenv/config'
+// import { PrismaClient } from '@prisma/client'
+// import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+
+// const adapter = new PrismaMariaDb({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+//   connectionLimit: 5,
+//   // 👇 THIS IS REQUIRED FOR MYSQL 8
+//   allowPublicKeyRetrieval: true 
+// })
+
+// const prisma = global.prisma || new PrismaClient({ adapter });
+
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
-import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  connectionLimit: 5,
-  // 👇 THIS IS REQUIRED FOR MYSQL 8
-  allowPublicKeyRetrieval: true 
-})
+const connectionString = `${process.env.DATABASE_URL}`
 
-const prisma = global.prisma || new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
